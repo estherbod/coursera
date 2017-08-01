7f7d29088bdc6890358a4593f30f9f009e8d1c4e
Esther Bod <esther.bod@gmail.com>
2017-08-01 21:09:55 +0200

    Implement behaviour for checking lunch items.

--- spa-with-angularjs/module1/app.js
+++ spa-with-angularjs/module1/app.js
@@ -7,5 +7,20 @@ angular.module('LunchCheck', [])
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
 
+  $scope.checkLunch = function () {
+    var menu = $scope.menu
+    if (!menu || 0 === menu.length) {
+      $scope.message = "Please enter data first";
+    }
+    else {
+      var items = menu.split(",").length;
+      if (items <= 3) {
+        $scope.message = "Enjoy!";
+      }
+      else {
+        $scope.message = "Too much!";
+      }
+    }
+  }
 };
 })();
