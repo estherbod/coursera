0c30413ed42e4d1680e1aa5beec706f6677195a7
Esther Bod <esther.bod@gmail.com>
2017-08-11 14:54:31 +0200

    Add app.js with service and controllers holding shopping items.

--- spa-with-angularjs/module2/app.js
+++ spa-with-angularjs/module2/app.js
@@ -2,5 +2,41 @@
 'use strict';
 
 angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var toBuy = this;
+
+  toBuy.items = ShoppingListCheckOffService.getItemsToBuy();
+}
+
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var bought = this;
+
+ bought.items = ShoppingListCheckOffService.getItemsBought();
+}
+
+
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  // List of shopping items to buy and items bought already
+  var toBuy = [{name: "cookies", quantity: 10}, {name: "milk", quantity: 2}, {name: "coffee", quantity: 3}, {name: "bread", quantity: 1}, {name: "chips", quantity: 2}]
+  var bought = [];
+
+  service.getItemsToBuy = function () {
+    return toBuy;
+  };
+
+  service.getItemsBought = function () {
+    return bought;
+  };
+}
 
 })();
