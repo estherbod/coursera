05e7218e88210779ecd6bae42590fcf1e4f324f5
Esther Bod <esther.bod@gmail.com>
2017-08-18 21:00:03 +0200

    Add NarrowItDownController and MenuSearchService.

--- spa-with-angularjs/module3/app.js
+++ spa-with-angularjs/module3/app.js
@@ -2,5 +2,16 @@
 'use strict';
 
 angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController() {
+  var ctrl = this;
+}
+
+function MenuSearchService() {
+  var service = this;
+}
 
 })();
