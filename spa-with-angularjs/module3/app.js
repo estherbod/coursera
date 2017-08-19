03429fb136cd78381d10bc32988fb0256ec5734c
Esther Bod <esther.bod@gmail.com>
2017-08-19 12:21:30 +0200

    Show unfiltered list of items.

--- spa-with-angularjs/module3/app.js
+++ spa-with-angularjs/module3/app.js
@@ -6,12 +6,37 @@ angular.module('NarrowItDownApp', [])
 .service('MenuSearchService', MenuSearchService)
 
 NarrowItDownController.$inject = ['MenuSearchService'];
-function NarrowItDownController() {
+function NarrowItDownController(MenuSearchService) {
   var ctrl = this;
+
+  ctrl.findItems = function () {
+    var promise = MenuSearchService.getMatchedMenuItems();
+    promise.then(function (response) {
+      ctrl.foundItems = response;
+    })
+    .catch(function (error) {
+      console.log("Something went terribly wrong.");
+    });
+  };
 }
 
-function MenuSearchService() {
+MenuSearchService.$inject = ['$http'];
+function MenuSearchService($http) {
   var service = this;
+
+  service.getMatchedMenuItems = function (searchTerm) {
+
+    return $http({
+      url: ("https://davids-restaurant.herokuapp.com/menu_items.json"),
+    }).then(function (response) {
+      var foundItems = response.data["menu_items"];
+      // TODO Filter items on search term
+      return foundItems;
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+  };
 }
 
 })();
