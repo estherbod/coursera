51e7eae7411a38cf54a9b8833f59067d5b0c27b5
Esther Bod <esther.bod@gmail.com>
2017-08-26 14:21:56 +0200

    Add method to get categories from server.

--- spa-with-angularjs/module4/src/menudata.service.js
+++ spa-with-angularjs/module4/src/menudata.service.js
@@ -9,6 +9,17 @@ MenuDataService.$inject = ['$http']
 function MenuDataService($http) {
   var service = this;
 
+  service.getAllCategories = function () {
+    return $http({
+      url: ("https://davids-restaurant.herokuapp.com/categories.json")
+    }).then(function (response) {
+       return response.data;
+     })
+    .catch(function (error) {
+      console.log(error);
+    })
+  };
+
 }
 
 })();
